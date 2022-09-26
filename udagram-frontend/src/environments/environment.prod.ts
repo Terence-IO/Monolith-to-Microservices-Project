758f1daeb30559d47569aabad9acdfcdfe86ed5b
Terence <112216771+Terence-IO@users.noreply.github.com>
2022-09-26 04:03:25 +0300

    Added K8s files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a65a482cb9a52440ba62e2d6c49c0d0d-660964340.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
