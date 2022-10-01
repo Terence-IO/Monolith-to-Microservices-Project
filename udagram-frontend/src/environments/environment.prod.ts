f85218856674e69f86664005f77aa3b41883bd98
Terence <112216771+Terence-IO@users.noreply.github.com>
2022-10-01 18:32:46 +0300

    Changed the Logging and horizontal scaling screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af2aec39fb9dc487ca26365d0a23896a-758700266.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
